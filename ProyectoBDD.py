b954d3ed9585a2e43893ef54a3574e4ad896a5d9
Oscar De Leon <owdelc@gmail.com>
2021-05-21 12:20:50 -0600

    Avances
    Implementacion parcial de Admin. Implementacion total de SIgn Up y Mejoras de funcionamiento en Log In

--- ProyectoBDD.py
+++ ProyectoBDD.py
@@ -22,18 +22,20 @@ conn = psycopg2.connect (
 
 def Login(username,password):
     cur = conn.cursor()
-    cur.execute('select %s from users;', [username])
+    cur.execute('select name from users where name = %s;', [username])
     userp = [i [0] for i in cur.fetchall()]
+    print(userp)
     if userp == [username]:
-        cur.execute('select %s from users;',[password])
+        cur.execute('select password from users where password = %s;',[password])
         passp = [i [0] for i in cur.fetchall()]
+        print(passp)
         if passp == [password]:
-            cur.execute('select rol from users')
+            cur.execute('select rol from users where name = %s;',[username])
             roles = [i [0] for i in cur.fetchall()]
             if roles == [1]:
-                print("Admin user")
-                #AdminUser()
-            if roles == 'free':
+                print("*********************************")
+                AdminUser()
+            if roles == [2]:
                 print("Free user")
                 #FreeUser()
             if roles == 'premium':
@@ -51,13 +53,45 @@ def Login(username,password):
         print("Username is incorrect.")
 
 
-def SignUp(username,password1,password2):
+def SignUp():
+    username = input("Ingrese usuario: ")
+    password1 = input("Ingrese contraseña: ")
+    password2 = input("Confirme contraseña: ")
+    tipo = 2
     if password1 == password2:
-        cur = conn.cursor()
-        cur.execute('insert into users (nombre,password) values (%s,%s);' [username,password1])
-        print("Registro Exitoso")
-        Login()
+        try:
+            cur = conn.cursor()
+            cur.execute('INSERT INTO users(name,password,rol) VALUES(%s,%s,%s);',(username, password1, tipo))
+            conn.commit()
+            cur.close()
+        except (Exception, psycopg2.DatabaseError) as error:
+            print("Error en la transaccion, revirtiendo operaciones ", error)
+            conn.rollback()
 
+# Definicion de funcionalidades de Administrador 
+def AdminUser():
+    a = 0
+    while a != 6:
+        print('Ingrese la operacion que desea realizar')
+        print('1. Agregar un usuario \n' + '2. Actualizar usuario \n' + '3. Eliminar Usuario \n' + '4. Ver vitacora de acciones \n' + '5. Realizar simulacion de reproducciones \n' + '6. Salir \n')
+        a = int(input('La operacion que desea realizar: '))
+        
+        if a == 1:
+            usuario = input('Ingrese usuario que desea registrar: ')
+            password1 = input('Ingrese contraseña: ')
+            password2 = input('Confirme contraseña: ')
+            print('Debe designar un tipo de usuario. Las opciones son las siguientes: \n' + '1. Administrador \n' + '2. Freemium \n' + '3. Premium \n' + '4. Artista/Manager')
+            tipo = int(input('Tipo de usuario: '))
+            
+        
+        if a == 2:
+            print("opcion")
+        if a == 3:
+            print("opcion")
+        if a == 4:
+            print("opcion")
+        if a == 5:
+            print("opcion")
 
 #Log in y Register 
 
@@ -71,9 +105,7 @@ if opcion1 == 1:
     Login(username, password)
     
 if opcion1 == 2:
-    username = input("Ingrese usuario: ")
-    password1 = input("Ingrese contraseña: ")
-    password2 = input("Confirme contraseña: ")
-    SignUp(username, password1, password2)
+    
+    SignUp()
     
     
